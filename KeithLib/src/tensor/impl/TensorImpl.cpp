e749ed0d0ed7d5d69b24f8f850c357cde1288227
Linus Honer <lhon2701@gmail.com>
2024-09-02 02:04:04 -0500

    alloc

--- KeithLib/src/tensor/impl/TensorImpl.cpp
+++ KeithLib/src/tensor/impl/TensorImpl.cpp
@@ -110,7 +110,130 @@ namespace keith {
     }
 
 
+    Alloc::NonTrivalUniquePtr<TensorImpl>
+        TensorImpl::slice(index_t idx, index_t dim) const {
+        CHECK_IN_RANGE(dim, 0, n_dim(),
+            "Dimension out of range (expected to be in range of [0, %d), but got %d)",
+            n_dim(), dim);
+        CHECK_IN_RANGE(idx, 0, size(dim),
+            "Index %d is out of bound for dimension %d with size %d",
+            idx, dim, size(dim));
+        Alloc::NonTrivalUniquePtr<TensorImpl> ptr;
+        ptr = Alloc::unique_construct<TensorImpl>(
+            Storage(_storage, offset() + _stride[dim] * idx),
+            _shape, _stride);
+        ptr->_shape[dim] = 1;
+        ptr->_stride[dim] = 0;
+        return ptr;
+    }
 
+    Alloc::NonTrivalUniquePtr<TensorImpl>
+        TensorImpl::slice(index_t start_idx, index_t end_idx, index_t dim) const {
+        CHECK_IN_RANGE(dim, 0, n_dim(),
+            "Dimension out of range (expected to be in range of [0, %d), but got %d)",
+            n_dim(), dim);
+        CHECK_IN_RANGE(start_idx, 0, size(dim),
+            "Index %d is out of bound for dimension %d with size %d",
+            start_idx, dim, size(dim));
+        CHECK_IN_RANGE(end_idx, 0, size(dim) + 1,
+            "Range end %d is out of bound for dimension %d with size %d",
+            end_idx, dim, size(dim));
+        CHECK_TRUE(start_idx < end_idx,
+            "slice() expects the start index must be smaller than the end index");
+        Alloc::NonTrivalUniquePtr<TensorImpl> ptr;
+        ptr = Alloc::unique_construct<TensorImpl>(
+            Storage(_storage, offset() + start_idx * _stride[dim]),
+            _shape, _stride);
+        ptr->_shape[dim] = end_idx - start_idx;
+        return ptr;
+    }
+
+    Alloc::NonTrivalUniquePtr<TensorImpl>
+        TensorImpl::transpose(index_t dim1, index_t dim2) const {
+        CHECK_IN_RANGE(dim1, 0, n_dim(),
+            "Dimension out of range (expected to be in range of [0, %d), but got %d)",
+            n_dim(), dim1);
+        CHECK_IN_RANGE(dim2, 0, n_dim(),
+            "Dimension out of range (expected to be in range of [0, %d), but got %d)",
+            n_dim(), dim2);
+        Alloc::NonTrivalUniquePtr<TensorImpl> ptr;
+        ptr = Alloc::unique_construct<TensorImpl>(_storage, _shape, _stride);
+        std::swap(ptr->_shape[dim1], ptr->_shape[dim2]);
+        std::swap(ptr->_stride[dim1], ptr->_stride[dim2]);
+        return ptr;
+    }
+
+    Alloc::NonTrivalUniquePtr<TensorImpl>
+        TensorImpl::view(const Shape& shape) const {
+        CHECK_TRUE(!is_contiguous(),
+            "view() is only supported to contiguous tensor");
+        CHECK_EQUAL(shape.d_size(), shape.d_size(),
+            "Shape of size %d is invalid for input tensor with size %d",
+            shape.d_size(), shape.d_size());
+        Alloc::NonTrivalUniquePtr<TensorImpl> ptr;
+        ptr = Alloc::unique_construct<TensorImpl>(_storage, shape);
+        for (int i = 0; i < shape.n_dim(); ++i) {
+            if (i + 1 < shape.n_dim()) ptr->_stride[i] = shape.sub_size(i + 1);
+            else ptr->_stride[i] = 1;
+            if (shape[i] == 1) ptr->_stride[i] = 0;
+        }
+        return ptr;
+    }
+
+    Alloc::NonTrivalUniquePtr<TensorImpl>
+        TensorImpl::permute(std::initializer_list<index_t> dims) const {
+        CHECK_EQUAL(dims.size(), n_dim(),
+            "Dimension not match (expected dims of %d, but got %zu)",
+            n_dim(), dims.size());
+        Alloc::NonTrivalUniquePtr<TensorImpl> ptr;
+        ptr = Alloc::unique_construct<TensorImpl>(_storage, _shape);
+        int idx = 0;
+        for (auto n_permute : dims) {
+            ptr->_shape[idx] = _shape[n_permute];
+            ptr->_stride[idx] = _stride[n_permute];
+            ++idx;
+        }
+        return ptr;
+    }
+
+    Alloc::NonTrivalUniquePtr<TensorImpl>
+        TensorImpl::sum(int idx) const {
+        CHECK_IN_RANGE(idx, 0, n_dim(),
+            "Dimension out of range (expected to be in range of [0, %d), but got %d)",
+            n_dim(), idx);
+        Alloc::NonTrivalUniquePtr<TensorImpl> ptr;
+        ptr = Alloc::unique_construct<TensorImpl>(Shape(_shape, idx));
+        std::vector<index_t> idxs(_shape.n_dim(), 0);
+        std::vector<index_t> new_idxs(_shape.n_dim() - 1, 0);
+        index_t cnt = 0;
+        while (cnt < d_size()) {
+            data_t res = 0;
+            for (int i = 0; i < _shape[idx]; ++i) {
+                idxs[idx] = i;
+                res += eval(idxs);
+            }
+            new_idxs.clear();
+            for (int i = 0; i < n_dim(); ++i) {
+                if (i == idx) continue;
+                new_idxs.push_back(idxs[i]);
+            }
+            index_t index = 0;
+            for (index_t i = 0; i < new_idxs.size(); ++i) {
+                index += new_idxs[i] * ptr->_stride[i];
+            }
+            ptr->item(index) = res;
+            for (int i = 0; i < n_dim(); ++i) {
+                if (i == idx) continue;
+                if (idxs[i] + 1 >= _shape[i]) idxs[i] = 0;
+                else {
+                    ++idxs[i];
+                    break;
+                }
+            }
+            cnt += _shape[idx];
+        }
+        return ptr;
+    }
 
 
 
