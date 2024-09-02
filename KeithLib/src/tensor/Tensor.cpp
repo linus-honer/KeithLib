d45d1af1ae75924d2be3f8e06a886fbc0a8a0e6d
Linus Honer <lhon2701@gmail.com>
2024-09-02 02:07:47 -0500

    oxyphinbhutazonfene

--- KeithLib/src/tensor/Tensor.cpp
+++ KeithLib/src/tensor/Tensor.cpp
@@ -1,1 +1,14 @@
 #include "Tensor.h"
+
+#include <memory>
+
+namespace keith {
+
+	Tensor::Tensor(const Storage& storage, const Shape& shape, const Array<index_t>& stride) : Exp<TensorImpl>(Alloc::unique_construct<TensorImpl>(storage, shape, stride)) {}
+	Tensor::Tensor(const Storage& storage, const Shape& shape) : Exp<TensorImpl>(Alloc::unique_construct<TensorImpl>(storage, shape)) {}
+	Tensor::Tensor(const Shape& shape) : Exp<TensorImpl>(Alloc::unique_construct<TensorImpl>(shape)) {}
+	Tensor::Tensor(const data_t* data, const Shape& shape) : Exp<TensorImpl>(Alloc::unique_construct<TensorImpl>(data, shape)) {}
+	Tensor::Tensor(Storage&& storage, Shape&& shape, Array<index_t>&& stride) : Exp<TensorImpl>(Alloc::unique_construct<TensorImpl>(std::move(storage), std::move(shape), std::move(stride))) {}
+	Tensor::Tensor(Alloc::NonTrivalUniquePtr<TensorImpl>&& ptr) : Exp<TensorImpl>(std::move(ptr)) {}
+
+}
--- KeithLib/src/tensor/Tensor.h
+++ KeithLib/src/tensor/Tensor.h
@@ -6,6 +6,24 @@ namespace keith {
 	class Tensor : public Exp<TensorImpl>
 	{
 		using Exp<TensorImpl>::impl_ptr;
+	public:
+		Tensor(const Storage& storage, const Shape& shape, const Array<index_t>& stride);
+		Tensor(const Storage& storage, const Shape& shape);
+		explicit Tensor(const Shape& shape);
+		Tensor(const data_t* data, const Shape& shape);
+		Tensor(Storage&& storage, Shape&& shape, Array<index_t>&& stride);
+		Tensor(const Tensor& other) = default;
+		Tensor(Tensor&& other) = default;
+		Tensor& operator=(const Tensor& other)
+		{
+			if (this != &other) {
+				impl_ptr = Alloc::unique_construct<TensorImpl>(*other.impl_ptr);
+			}
+			return *this;
+		}
+		Tensor& operator=(Tensor&& other) = default;
+		~Tensor() = default;
+		explicit Tensor(Alloc::NonTrivalUniquePtr<TensorImpl>&& ptr);
 	};
 
 }
