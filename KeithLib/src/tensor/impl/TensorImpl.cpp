f156d7a89942a5890d361c17ec4aff8f71003c13
Linus Honer <lhon2701@gmail.com>
2024-09-02 02:02:27 -0500

    fren

--- KeithLib/src/tensor/impl/TensorImpl.cpp
+++ KeithLib/src/tensor/impl/TensorImpl.cpp
@@ -108,4 +108,123 @@ namespace keith {
         }
         return item(index);
     }
+
+
+
+
+
+
+    std::ostream& operator<<(std::ostream& out, const TensorImpl& tensor) {
+        int max_width = 0;
+        for (int i = 0; i < tensor.d_size(); ++i) {
+            int value = (int)std::abs(tensor.item(i));
+            int dig = value = (int)(std::log10(value)) + 1;
+            if (tensor.item(i) < 0) ++dig;
+            max_width = std::max(max_width, dig);
+        }
+        int cnt = 0, idx = 0, end_flag = tensor.n_dim();
+        std::vector<int> dim_cnt(tensor.n_dim());
+        while (cnt < tensor.d_size()) {
+            for (int i = 0; i < tensor.n_dim() - end_flag; ++i)
+                out << " ";
+            for (int i = 0; i < end_flag; ++i)
+                out << "[";
+            out << std::setw(max_width + 4 + 1) << std::right << std::setprecision(4) << std::fixed;
+            out << tensor.item(idx);
+            end_flag = 0;
+            for (int i = (int)tensor.n_dim() - 1; i >= 0; --i) {
+                if (dim_cnt[i] + 1 < tensor.size()[i]) {
+                    idx += tensor.stride()[i];
+                    ++dim_cnt[i];
+                    break;
+                }
+                else {
+                    idx -= ((int)tensor.size()[i] - 1) * tensor.stride()[i];
+                    dim_cnt[i] = 0;
+                    ++end_flag;
+                }
+            }
+            if (end_flag == 0) out << ", ";
+            else {
+                for (int i = 0; i < end_flag; ++i) {
+                    out << "]";
+                }
+                out << std::endl;
+
+            }
+            ++cnt;
+        }
+        return out;
+    }
+
+    data_t TensorImpl::sum() const {
+        data_t res = 0;
+        std::vector<index_t> idx(n_dim(), 0);
+        for (int i = 0; i < d_size(); ++i) {
+            int cnt = 0;
+            res += eval(idx);
+            for (int j = 0; j < n_dim(); ++j) {
+                if (idx[j] + 1 < size()[j]) {
+                    ++idx[j];
+                    break;
+                }
+                else {
+                    idx[j] = 0;
+                    ++cnt;
+                }
+            }
+            if (cnt == n_dim()) break;
+        }
+        return res;
+    }
+
+    TensorImpl TensorMaker::ones(const Shape& shape) {
+        TensorImpl tensor(shape);
+        for (int i = 0; i < tensor.d_size(); ++i)
+            tensor.item(i) = 1;
+        return tensor;
+    }
+
+    TensorImpl TensorMaker::ones_like(const TensorImpl& tensor) {
+        return ones(tensor.size());
+    }
+
+    TensorImpl TensorMaker::zeros(const Shape& shape) {
+        TensorImpl tensor(shape);
+        for (int i = 0; i < tensor.d_size(); ++i)
+            tensor.item(i) = 0;
+        return tensor;
+    }
+
+    TensorImpl TensorMaker::zeros_like(const TensorImpl& tensor) {
+        return zeros(tensor.size());
+    }
+
+    TensorImpl TensorMaker::rand(const Shape& shape) {
+        std::random_device rd;
+        std::default_random_engine gen(rd());
+        std::uniform_real_distribution<data_t> dis(0, 1);
+        TensorImpl tensor(shape);
+        for (int i = 0; i < tensor.d_size(); ++i)
+            tensor.item(i) = dis(gen);
+        return tensor;
+    }
+
+    TensorImpl TensorMaker::rand_like(const TensorImpl& tensor) {
+        return rand(tensor.size());
+    }
+
+    TensorImpl TensorMaker::randn(const Shape& shape) {
+        std::random_device rd;
+        std::default_random_engine gen(rd());
+        std::normal_distribution<data_t> dis(0, 1);
+        TensorImpl tensor(shape);
+        for (int i = 0; i < tensor.d_size(); ++i)
+            tensor.item(i) = dis(gen);
+        return tensor;
+    }
+
+    TensorImpl TensorMaker::randn_like(const TensorImpl& tensor) {
+        return randn(tensor.size());
+    }
 }
